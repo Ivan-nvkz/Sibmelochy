0ce0fdf002c950ad550ea24e1cce8615aecfc9f9
Ivan-nvkz <66069539+Ivan-nvkz@users.noreply.github.com>
2022-04-12 17:19:37 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,54 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+   // Slider news-slider start =====================================================================================
+   const news = new Swiper('.news-slider', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 4,
+      spaceBetween: 30,
+      slidesPerGroup: 1,
+      speed: 800,
+
+      // Responsive breakpoints
+      breakpoints: {
+         // when window width is >= 320px
+         320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+         },
+         // when window width is >= 480px
+         480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+         },
+         // when window width is >= 640px
+         640: {
+            slidesPerView: 3,
+            spaceBetween: 30
+         },
+         800: {
+            slidesPerView: 4,
+            spaceBetween: 30
+         },
+         1000: {
+            slidesPerView: 4,
+            spaceBetween: 30
+         },
+         1200: {
+            slidesPerView: 4,
+            spaceBetween: 30
+         }
+      },
+
+      // Navigation arrows
+      navigation: {
+         nextEl: '.news__button-next',
+         prevEl: '.news__button-prev',
+      },
+   });
+   // Slider news-slider end =======================================================================================
 
 
    // ======================================================================================
